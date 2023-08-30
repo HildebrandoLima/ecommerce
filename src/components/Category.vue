0c4569370c6371ba2cdffe51493b024056d7f99f
Hildebrando Alves <hildebrandolima16@gmail.com>
2023-08-30 10:51:22 -0300

    Comunicação via api da categorias

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -3,7 +3,37 @@
 </script>
 
 <template>
-    <li><RouterLink class="dropdown-item" to="/">Action</RouterLink></li>
-    <li><RouterLink class="dropdown-item" to="/">Another action</RouterLink></li>
-    <li><RouterLink class="dropdown-item" to="/">Something else here</RouterLink></li>
+    <li v-for="(category, index) in categories" :key="index"><RouterLink class="dropdown-item" to="{{ category.nome }}">{{ category.nome }}</RouterLink></li>
 </template>
+
+<script>
+  import api from '@/api';
+
+  export default {
+    name: 'category',
+    errorList: {},
+    data() {
+      return {
+        categories: {},
+      };
+    },
+    created() {
+        this.getCategory();
+    },
+    methods: {
+      getCategory() {
+        api.get('/category/list?active=1')
+          .then((response) => {
+            console.log(response.data.data)
+            this.categories = response.data.data
+          })
+          .catch((error) => {
+            if (error.response.data.status === 400) {
+                console.log(error.response.data.data)
+                this.errorList = error.response.data.data
+            }
+          });
+      },
+    },
+};
+</script>
