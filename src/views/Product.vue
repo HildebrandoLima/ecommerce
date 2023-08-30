412406e665042cddf1c1b0c96fb9fe524007aae6
Hildebrando Alves <hildebrandolima16@gmail.com>
2023-08-30 11:31:29 -0300

    Comunicação via api para produtos

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -57,140 +57,63 @@
         </header>
 
         <div class="row">
-          <div class="col-lg-4 col-md-6 col-sm-6 d-flex">
+          <div v-for="(product, index) in products.list" :key="index" class="col-lg-4 col-md-6 col-sm-6 d-flex">
             <div class="card w-100 my-2 shadow-2-strong">
               <RouterLink to="productDetails">
                 <img src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/10.webp" class="card-img-top">
               </RouterLink>
               <div class="card-body d-flex flex-column">
                 <div class="d-flex flex-row">
-                  DE: <h5 class="mb-1 me-1">R$34,50</h5>
-                  POR: <span class="text-danger"><s>R$49,99</s></span>
+                  DE: <h5 class="mb-1 me-1">{{ product.precoCusto.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'}) }}</h5>
+                  POR: <span class="text-danger"><s>{{ product.precoVenda.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'}) }}</s></span>
                 </div>
 
-                <p class="card-text">T-shirts with multiple colors, for men and lady</p>
+                <p class="card-text">{{ product.nome }}</p>
                 <div class="card-footer d-flex align-items-end pt-3 px-0 pb-3 mt-auto">                  
                   <RouterLink to="/cart" class="btn btn-outline-primary shadow-0 me-1">Adicionar ao carrinho</RouterLink>
                   <RouterLink to="/" class="btn btn-danger border px-2 pt-2 icon-hover"><i class="fas fa-heart fa-lg text-white px-1"></i></RouterLink>
                 </div>
               </div>
             </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 col-sm-6 d-flex">
-            <div class="card w-100 my-2 shadow-2-strong">
-              <img src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/11.webp" class="card-img-top">
-              <div class="card-body d-flex flex-column">
-                <h5 class="card-title">$120.00</h5>
-                <p class="card-text">Winter Jacket for Men and Women, All sizes</p>
-                <div class="card-footer d-flex align-items-end pt-3 px-0 pb-0 mt-auto">
-                  <RouterLink to="/cart" class="btn btn-primary shadow-0 me-1">Adicionar ao carrinho</RouterLink>
-                  <RouterLink to="/" class="btn btn-danger border px-2 pt-2 icon-hover"><i class="fas fa-heart fa-lg text-white px-1"></i></RouterLink>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 col-sm-6 d-flex">
-            <div class="card w-100 my-2 shadow-2-strong">
-              <img src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/12.webp" class="card-img-top">
-              <div class="card-body d-flex flex-column">
-                <h5 class="card-title">$120.00</h5>
-                <p class="card-text">T-shirts with multiple colors, for men and lady</p>
-                <div class="card-footer d-flex align-items-end pt-3 px-0 pb-0 mt-auto">
-                  <RouterLink to="/cart" class="btn btn-primary shadow-0 me-1">Adicionar ao carrinho</RouterLink>
-                  <RouterLink to="/" class="btn btn-danger border px-2 pt-2 icon-hover"><i class="fas fa-heart fa-lg text-white px-1"></i></RouterLink>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 col-sm-6 d-flex">
-            <div class="card w-100 my-2 shadow-2-strong">
-              <img src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/13.webp" class="card-img-top" style="aspect-ratio: 1 / 1">
-              <div class="card-body d-flex flex-column">
-                <h5 class="card-title">$120.00</h5>
-                <p class="card-text">Blazer Suit Dress Jacket for Men, Blue color</p>
-                <div class="card-footer d-flex align-items-end pt-3 px-0 pb-0 mt-auto">
-                  <a href="#!" class="btn btn-primary shadow-0 me-1">Add to cart</a>
-                  <a href="#!" class="btn btn-light border icon-hover px-2 pt-2"><i class="fas fa-heart fa-lg text-secondary px-1"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 col-sm-6 d-flex">
-            <div class="card w-100 my-2 shadow-2-strong">
-              <img src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/14.webp" class="card-img-top" style="aspect-ratio: 1 / 1">
-              <div class="card-body d-flex flex-column">
-                <h5 class="card-title">$510.00</h5>
-                <p class="card-text">Slim sleeve wallet Italian leather - multiple colors</p>
-                <div class="card-footer d-flex align-items-end pt-3 px-0 pb-0 mt-auto">
-                  <a href="#!" class="btn btn-primary shadow-0 me-1">Add to cart</a>
-                  <a href="#!" class="btn btn-light border icon-hover px-2 pt-2"><i class="fas fa-heart fa-lg text-secondary px-1"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-6 col-sm-6 d-flex">
-            <div class="card w-100 my-2 shadow-2-strong">
-              <img src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/10.webp" class="card-img-top">
-              <div class="card-body d-flex flex-column">
-                <h5 class="card-title">$79.99</h5>
-                <p class="card-text">T-shirts with multiple colors, for men and lady</p>
-                <div class="card-footer d-flex align-items-end pt-3 px-0 pb-0 mt-auto">
-                  <a href="#!" class="btn btn-primary shadow-0 me-1">Add to cart</a>
-                  <a href="#!" class="btn btn-light border icon-hover px-2 pt-2"><i class="fas fa-heart fa-lg text-secondary px-1"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-6 col-sm-6 d-flex">
-            <div class="card w-100 my-2 shadow-2-strong">
-              <img src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/11.webp" class="card-img-top">
-              <div class="card-body d-flex flex-column">
-                <h5 class="card-title">$120.00</h5>
-                <p class="card-text">Winter Jacket for Men and Women, All sizes</p>
-                <div class="card-footer d-flex align-items-end pt-3 px-0 pb-0 mt-auto">
-                  <a href="#!" class="btn btn-primary shadow-0 me-1">Add to cart</a>
-                  <a href="#!" class="btn btn-light border icon-hover px-2 pt-2"><i class="fas fa-heart fa-lg text-secondary px-1"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-6 col-sm-6 d-flex">
-            <div class="card w-100 my-2 shadow-2-strong">
-              <img src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/12.webp" class="card-img-top">
-              <div class="card-body d-flex flex-column">
-                <h5 class="card-title">$120.00</h5>
-                <p class="card-text">T-shirts with multiple colors, for men and lady</p>
-                <div class="card-footer d-flex align-items-end pt-3 px-0 pb-0 mt-auto">
-                  <a href="#!" class="btn btn-primary shadow-0 me-1">Add to cart</a>
-                  <a href="#!" class="btn btn-light border icon-hover px-2 pt-2"><i class="fas fa-heart fa-lg text-secondary px-1"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-6 col-sm-6 d-flex">
-            <div class="card w-100 my-2 shadow-2-strong">
-              <img src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/9.webp" class="card-img-top">
-              <div class="card-body d-flex flex-column">
-                <h5 class="card-title">$43.50</h5>
-                <p class="card-text">Summer New Men's Denim Jeans Shorts</p>
-                <div class="card-footer d-flex align-items-end pt-3 px-0 pb-0 mt-auto">
-                  <a href="#!" class="btn btn-primary shadow-0 me-1">Add to cart</a>
-                  <a href="#!" class="btn btn-light border icon-hover px-2 pt-2"><i class="fas fa-heart fa-lg text-secondary px-1"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
+          </div>         
         </div>
 
-        <hr>
-
+        <hr />
         <Pagination></Pagination>
       </div>
     </div>
   </div>            
 </section>
 </template>
+
+<script>
+  import api from '@/api';
+
+  export default {
+    name: 'product',
+    errorList: {},
+    data() {
+      return {
+        products: {},
+      };
+    },
+    created() {
+        this.getProduct();
+    },
+    methods: {
+      getProduct() {
+        api.get('/product/list?page=1&perPage=10&active=1')
+          .then((response) => {
+            console.log(response.data.data)
+            this.products = response.data.data
+          })
+          .catch((error) => {
+            if (error.response.data.status === 400) {
+                console.log(error.response.data.data)
+                this.errorList = error.response.data.data
+            }
+          });
+      },
+    },
+};
+</script>
