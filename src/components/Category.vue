4720847ca174294bbfee8fc2b185ab54fad1edca
Hildebrando Alves <hildebrandolima16@gmail.com>
2023-09-01 11:31:13 -0300

    Listagem de categorias com api

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,5 +1,9 @@
 <template>
-    <li v-for="(category, index) in categories" :key="index"><RouterLink class="dropdown-item" to="{{ category.nome }}">{{ category.nome }}</RouterLink></li>
+    <li v-for="(category, index) in categories" :key="index">
+      <RouterLink class="dropdown-item" to="{{ category.nome }}">
+        {{ category.nome }}
+      </RouterLink>
+    </li>
 </template>
 
 <script>
@@ -7,7 +11,7 @@
   import api from '@/api';
 
   export default {
-    components: {RouterLink},
+    components: { RouterLink },
     name: 'category',
     errorList: {},
     data() {
@@ -22,12 +26,10 @@
       getCategory() {
         api.get('/category/list?active=1')
           .then((response) => {
-            console.log(response.data.data)
             this.categories = response.data.data
           })
           .catch((error) => {
             if (error.response.data.status === 400) {
-                console.log(error.response.data.data)
                 this.errorList = error.response.data.data
             }
           });
