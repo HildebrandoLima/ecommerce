6ddd5a1cb01f90ac7941a12b21c565446c9eedc2
Hildebrando Alves <hildebrandolima16@gmail.com>
2023-09-09 12:43:01 -0300

    Aplicado camada de serviço

--- src/services/user/UserService.js
+++ src/services/user/UserService.js
@@ -4,7 +4,7 @@ export default class UserService {
     static async postUser(body) {
         try {
           const response = await api.post(`/user/save`, body);
-          localStorage.setItem('usuario', response.data.data);
+          localStorage.setItem('user', response.data.data);
           return response.data.message;
         } catch (error) {
           throw error;
