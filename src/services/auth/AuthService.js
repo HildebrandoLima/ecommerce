6a66bf82608a0408a747f429693519104495f1e1
Hildebrando Alves <hildebrandolima16@gmail.com>
2023-09-15 11:58:55 -0300

    Login social

--- src/services/auth/AuthService.js
+++ src/services/auth/AuthService.js
@@ -4,7 +4,7 @@ export default class AuthService {
     static async login(body) {
         try {
             const response = await api.post(`/auth/login`, body);
-            localStorage.setItem('userAuh', JSON.stringify(response.data.data));
+            window.localStorage.setItem('userAuh', JSON.stringify(response.data.data));
             return response.data.message;
         } catch (error) {
             throw error;
@@ -14,7 +14,22 @@ export default class AuthService {
     static async logout() {
         try {
             const response = await api.post(`/auth/logout`);
-            return response.data.data;
+            if (response.data.status === 200) {
+                window.localStorage.removeItem('userAuh');
+            }
+            return response.data.message;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async loginSocial(provider) {
+        try {
+            const response = await api.get(`/auth/login/social/${provider}`);
+            //console.log('oi', response);
+            //const response = await api.get(`/auth/login/social/${provider}/callback`);
+            window.localStorage.setItem('userAuh', JSON.stringify(response.data));
+            return response.data.message;
         } catch (error) {
             throw error;
         }
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,7 +6,7 @@
             <div class="card-body">
                 <div class="row">
                     <div class="col">
-                        <form @submit.prevent="logar">
+                        <form @submit.prevent="auth">
                             <div class="form-outline mb-4">
                                 <input type="email" id="email" v-model="user.email" class="form-control" placeholder="E-mail" required />
                             </div>
@@ -44,17 +44,21 @@
                         <div class="text-center">
                             <p>Não possue uma conta? <RouterLink to="/account">Registre-se</RouterLink></p>
                             <p>ou cadastre-se com:</p>
-                            <button type="button" class="btn btn-secondary btn-floating mx-1">
+                            <button type="button" @click="oAuth('facebook')" class="btn btn-secondary btn-floating mx-1">
                                 <i class="fab fa-facebook-f px-20"></i>
                             </button>
 
-                            <button type="button" class="btn btn-secondary btn-floating mx-1">
+                            <button type="button" @click="oAuth('google')" class="btn btn-secondary btn-floating mx-1">
                                 <i class="fab fa-google px-20"></i>
                             </button>
 
-                            <button type="button" class="btn btn-secondary btn-floating mx-1">
+                            <button type="button" @click="oAuth('github')" class="btn btn-secondary btn-floating mx-1">
                                 <i class="fab fa-github px-20"></i>
                             </button>
+
+                            <div v-if="Object.keys(this.errorList).length > 0" class="alert alert-danger mt-2" role="alert">
+                                {{ this.errorList }}
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -90,6 +94,11 @@
                     email: 'hildebrandolima16@gmail.com',
                     password: 'HiLd3br@ndo',
                 },
+                provider: {
+                    facebook: 'facebook',
+                    google: 'google',
+                    github: 'github',
+                },
                 products: {},
                 currentPage: 1,
                 perPage: 10,
@@ -111,10 +120,23 @@
                     }
                 }
             },
-            async logar() {
+            async auth() {
                 try {
                     const user = await AuthService.login(this.user);
                     this.messageSuccess = user;
+                } catch (error) {
+                    if (error.response && error.response.data.status === 400) {
+                        this.errorList = error.response.data.data;
+                    }
+                }
+            },
+            async oAuth(providerName) {
+                try {
+                    const providerValue = this.provider[providerName];
+                    const response = window.location.href = `http://localhost:8000/api/auth/login/social/${providerValue}`;
+                    window.localStorage.setItem('userAuh', JSON.stringify(response.data));
+                    //const provider = await AuthService.loginSocial(providerValue);
+                    //this.messageSuccess = provider;
                 } catch (error) {
                     if (error.response && error.response.data.status === 400) {
                         this.errorList = error.response.data.data;
