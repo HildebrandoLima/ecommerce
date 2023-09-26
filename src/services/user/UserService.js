d737d16e1df63b810ebe0d7bbd8210c8d744f415
Hildebrando Alves <hildebrandolima16@gmail.com>
2023-09-26 15:05:23 -0300

    Listagem única de usuário

--- src/services/user/UserService.js
+++ src/services/user/UserService.js
@@ -11,4 +11,13 @@ export default class UserService {
           throw error;
         }
     }
+
+    static async getUser(id) {
+      try {
+        const response = await api.get(`/user/list/find?id=${id}&active=1`);
+        return response.data.data;
+      } catch (error) {
+        throw error;
+      }
+  }
 }
