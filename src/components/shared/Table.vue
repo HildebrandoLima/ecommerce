a7f2d4da6443c1d15ce12883c943c5609504900e
Hildebrando Alves <hildebrandolima16@gmail.com>
2023-10-09 12:04:34 -0300

    Identação

--- src/components/shared/Table.vue
+++ src/components/shared/Table.vue
@@ -1,80 +1,80 @@
 <template>
-    <div class="table-responsive">
-        <table class="table table-striped table-hover">
-            <thead>
-                <tr>
-                    <th v-for="(column, index) in columns" :key="index">{{ formatColumn(column) }}</th>
-                    <th v-if="displayEdit">Ação</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(item, index) in data" :key="index">
-                    <td v-for="(column, columnIndex) in columns" :key="columnIndex">
-                        {{ item[column] }}
-                    </td>
+<div class="table-responsive">
+    <table class="table table-striped table-hover">
+        <thead>
+            <tr>
+                <th v-for="(column, index) in columns" :key="index">{{ formatColumn(column) }}</th>
+                <th v-if="displayEdit">Ação</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(item, index) in data" :key="index">
+                <td v-for="(column, columnIndex) in columns" :key="columnIndex">
+                    {{ item[column] }}
+                </td>
 
-                    <td v-if="displayEdit">
-                        <button
-                            @click="editItem(item)"
-                            class="btn btn-outline-primary border px-2 pt-2 icon-hover"
-                        >
-                        Editar
-                        </button>   
-                    </td>
+                <td v-if="displayEdit">
+                    <button
+                        @click="editItem(item)"
+                        class="btn btn-outline-primary border px-2 pt-2 icon-hover"
+                    >
+                    Editar
+                    </button>   
+                </td>
 
-                    <td v-if="displayEdit">
-                        <div class="form-check form-switch">
-                            <input
-                                type="checkbox"
-                                v-model="item.ativo"
-                                :id="'flexSwitchCheckChecked_' + index"
-                                :checked="item.ativo"
-                                class="form-check-input"
-                                disabled
-                            />
-                        </div>
-                    </td>
+                <td v-if="displayEdit">
+                    <div class="form-check form-switch">
+                        <input
+                            type="checkbox"
+                            v-model="item.ativo"
+                            :id="'flexSwitchCheckChecked_' + index"
+                            :checked="item.ativo"
+                            class="form-check-input"
+                            disabled
+                        />
+                    </div>
+                </td>
 
-                    <td v-if="displayModal">
-                        <button
-                            @click="addressModal(item.endereco)"
-                            class="btn btn-outline-primary border px-2 pt-2 icon-hover"
-                            data-toggle="modal"
-                            data-target="#editUserModal"
-                        >
-                        <i class="fas fa-address-book fa-lg text-dark px-1"></i>
-                        Endereço
-                        </button>
-                    </td>
+                <td v-if="displayModal">
+                    <button
+                        @click="addressModal(item.endereco)"
+                        class="btn btn-outline-primary border px-2 pt-2 icon-hover"
+                        data-toggle="modal"
+                        data-target="#editUserModal"
+                    >
+                    <i class="fas fa-address-book fa-lg text-dark px-1"></i>
+                    Endereço
+                    </button>
+                </td>
 
-                    <td v-if="displayModal">
-                        <button
-                            @click="itemModal(item.itens)"
-                            class="btn btn-outline-primary border px-2 pt-2 icon-hover"
-                            data-toggle="modal"
-                            data-target="#editUserModal"
-                        >
-                        <i class="fas fa-sitemap fa-lg text-dark px-1"></i>
-                        Item
-                        </button>
-                    </td>
+                <td v-if="displayModal">
+                    <button
+                        @click="itemModal(item.itens)"
+                        class="btn btn-outline-primary border px-2 pt-2 icon-hover"
+                        data-toggle="modal"
+                        data-target="#editUserModal"
+                    >
+                    <i class="fas fa-sitemap fa-lg text-dark px-1"></i>
+                    Item
+                    </button>
+                </td>
 
-                    <td v-if="displayModal">
-                        <button
-                            @click="paymentModal(item.pagamento)"
-                            class="btn btn-outline-primary border px-2 pt-2 icon-hover"
-                            data-toggle="modal"
-                            data-target="#editUserModal"
-                        >
-                        <i class="fas fa-credit-card fa-lg text-dark px-1"></i>
-                        Payment
-                        </button>
-                    </td>
+                <td v-if="displayModal">
+                    <button
+                        @click="paymentModal(item.pagamento)"
+                        class="btn btn-outline-primary border px-2 pt-2 icon-hover"
+                        data-toggle="modal"
+                        data-target="#editUserModal"
+                    >
+                    <i class="fas fa-credit-card fa-lg text-dark px-1"></i>
+                    Payment
+                    </button>
+                </td>
 
-                </tr>
-            </tbody>
-        </table>
-    </div>
+            </tr>
+        </tbody>
+    </table>
+</div>
 </template>
 
 <script>
