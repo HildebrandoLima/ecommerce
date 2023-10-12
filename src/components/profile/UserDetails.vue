1582e1904a599ad1a2439221bad1cafbfa79bef8
Hildebrando Alves <hildebrandolima16@gmail.com>
2023-10-12 11:48:11 -0300

    Refatorado view de perfil

---
+++ src/components/profile/UserDetails.vue
@@ -0,0 +1,88 @@
+<template>
+<div class="card border shadow-0" style="border-radius: 16px;">
+  <div class="m-4">
+    <div class="card-body">
+      <h5 class="card-title d-flex flex-column align-items-center text-center">
+        <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="" class="rounded-circle" width="150">
+        <p class="card-text text-center mt-2"><i class="fas fa-user fa-lg text-dark px-1"></i>{{ user.nome }}, {{ user.dataNascimento }} anos</p>
+      </h5>
+
+      <div class="card-body d-flex flex-column">
+        <div class="card-title text-muted">
+          <p class="card-text"><b>CPF:</b> {{ user.cpf }}</p>
+          <p class="card-text"><b>E-mail:</b> {{ user.email }}</p>
+
+          <div class="row">
+            <div class="col">
+              <p class="card-text">
+              <b>Verificado:</b>
+              <span v-if="user.emailVerificado">
+                <i class="fas fa-check-circle fa-lg text-success px-1"></i>
+              </span>
+              <span v-else>
+                <i class="fas fa-check-circle fa-lg text-danger px-1"></i>
+              </span>
+              </p>
+            </div>
+
+            <div class="col">
+              <p class="card-text">
+              <b>Ativo:</b>
+              <span v-if="user.ativo">
+                <i class="fas fa-check-circle fa-lg text-success px-1"></i>
+              </span>
+              <span v-else>
+                <i class="fas fa-check-circle fa-lg text-danger px-1"></i>
+              </span>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        <div class="d-flex align-items-end pt-3 px-0 pb-0 mt-auto">
+          <button @click="editItem(user)" class="btn btn-outline-primary border px-2 pt-2 icon-hover" data-toggle="modal" data-target="#editUserModal">
+            <i class="fas fa-user fa-lg text-dark px-1"></i>
+            Editar
+          </button>
+          &nbsp;&nbsp;&nbsp;
+          <button @click="showAddress" class="btn btn-outline-secondary border px-2 pt-2 icon-hover">
+            <i class="fas fa-address-book fa-lg text-dark px-1"></i>
+            Endereço
+          </button>
+          &nbsp;&nbsp;&nbsp;
+          <button @click="showPhone" class="btn btn-outline-warning border px-2 pt-2 icon-hover">
+            <i class="fas fa-phone fa-lg text-dark px-1"></i>
+            Telefone
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'profile-details',
+  props: {
+    user: {
+      type: Object,
+      required: true,
+      default: {},
+    },
+  },
+  methods: {
+    editItem(item) {
+      this.$emit('editItem', item);
+    },
+    showAddress(item) {
+      this.$emit('showAddress', item);
+    },
+    showPhone(item) {
+      this.$emit('showPhone', item);
+    },
+  },
+};
+</script>
--- src/views/client/profile/Profile.vue
+++ src/views/client/profile/Profile.vue
@@ -6,94 +6,23 @@
 
         <div class="row">
             <div class="col-lg-4">
-                <div class="card border shadow-0" style="border-radius: 16px;">
-                    <div class="m-4">
-                        <div class="card-body">
-                            <h5 class="card-title d-flex flex-column align-items-center text-center">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="" class="rounded-circle" width="150">
-                                <p class="card-text text-center mt-2"><i class="fas fa-user fa-lg text-dark px-1"></i>{{ user.nome }}, {{ user.dataNascimento }} anos</p>
-                            </h5>
-
-                            <div class="card-body d-flex flex-column">
-                                <div class="card-title text-muted">
-                                    <p class="card-text"><b>CPF:</b> {{ user.cpf }}</p>
-                                    <p class="card-text"><b>E-mail:</b> {{ user.email }}</p>
-
-                                    <div class="row">
-                                        <div class="col">
-                                            <p class="card-text">
-                                                <b>Verificado:</b>
-                                                <span v-if="user.emailVerificado">
-                                                    <i class="fas fa-check-circle fa-lg text-success px-1"></i>
-                                                </span>
-                                                <span v-else>
-                                                    <i class="fas fa-check-circle fa-lg text-danger px-1"></i>
-                                                </span>
-                                            </p>
-                                        </div>
-
-                                        <div class="col">
-                                            <p class="card-text">
-                                                <b>Ativo:</b>
-                                                <span v-if="user.ativo">
-                                                    <i class="fas fa-check-circle fa-lg text-success px-1"></i>
-                                                </span>
-                                                <span v-else>
-                                                    <i class="fas fa-check-circle fa-lg text-danger px-1"></i>
-                                                </span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <hr />
-
-                                <div class="d-flex align-items-end pt-3 px-0 pb-0 mt-auto">
-                                    <button @click="editItem(user)" class="btn btn-outline-primary border px-2 pt-2 icon-hover" data-toggle="modal" data-target="#editUserModal">
-                                        <i class="fas fa-user fa-lg text-dark px-1"></i>
-                                        Editar
-                                    </button>
-                                    &nbsp;&nbsp;&nbsp;
-                                    <button @click="showAddress" class="btn btn-outline-secondary border px-2 pt-2 icon-hover">
-                                        <i class="fas fa-address-book fa-lg text-dark px-1"></i>
-                                        Endereço
-                                    </button>
-                                    &nbsp;&nbsp;&nbsp;
-                                    <button @click="showPhone" class="btn btn-outline-warning border px-2 pt-2 icon-hover">
-                                        <i class="fas fa-phone fa-lg text-dark px-1"></i>
-                                        Telefone
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <UserDetails 
+                    :user="user"
+                    @editItem="editItem"
+                    @showAddress="showAddress"
+                    @showPhone="showPhone"
+                />
             </div>
 
             <div class="col-lg-8">
-                <div class="card border shadow-0" style="border-radius: 16px;">
-                    <div class="m-4">
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <Table
-                                    v-if="toggleAndressComponentVisibility"
-                                    :data="user.enderecos"
-                                    :columns="addressColumns"
-                                    :displayEdit="true"
-                                    @edit="editItem"
-                                />
-
-                                <Table
-                                    v-if="togglePhoneComponentVisibility"
-                                    :data="user.telefones"
-                                    :columns="telephoneColumns"
-                                    :displayEdit="true"
-                                    @edit="editItem"
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <UserContact
+                    :user="user"
+                    :toggleAddressComponentVisibility="toggleAddressComponentVisibility"
+                    :togglePhoneComponentVisibility="togglePhoneComponentVisibility"
+                    :addressColumns="addressColumns"
+                    :telephoneColumns="telephoneColumns"
+                    @edit="editItem"
+                />
             </div>
         </div>
 
@@ -113,13 +42,15 @@ import AddressEdit from '@/components/address/AddressEdit.vue';
 import Table from '@/components/shared/Table.vue';
 import TelephoneEdit from '@/components/telephone/TelephoneEdit.vue';
 import UserEdit from '@/components/user/UserEdit.vue';
+import UserContact from '@/components/profile/UserContact.vue';
+import UserDetails from '@/components/profile/UserDetails.vue';
 import UserService from '@/services/user/UserService';
 import { userAuth } from '@/storages/AuthStorage';
 import { USER_NOT_FOUND_MESSAGE } from '@/utils/defaultMessages/DefaultMessage';
 
 export default {
     name: 'profile',
-    components: { Banner, AddressEdit, Table, TelephoneEdit, UserEdit },
+    components: { Banner, AddressEdit, Table, TelephoneEdit, UserEdit, UserContact, UserDetails },
     data() {
         return {
             bannerTitleMessage: 'Meu Perfil',
@@ -129,7 +60,7 @@ export default {
             userEmail: '',
             user: {},
             editedItem: {},
-            toggleAndressComponentVisibility: true,
+            toggleAddressComponentVisibility: true,
             togglePhoneComponentVisibility: false,
             addressColumns: ['logradouro', 'numero', 'bairro', 'cidade', 'uf'],
             telephoneColumns: ['numero', 'tipo'],
@@ -144,11 +75,11 @@ export default {
     },
     methods: {
         showAddress() {
-            this.toggleAndressComponentVisibility = true;
+            this.toggleAddressComponentVisibility = true;
             this.togglePhoneComponentVisibility = false;
         },
         showPhone() {
-            this.toggleAndressComponentVisibility = false;
+            this.toggleAddressComponentVisibility = false;
             this.togglePhoneComponentVisibility = true;
         },
         async getUser() {
