13cd8456f5b384b82473ae9957147ce7242756c9
Hildebrando Alves <hildebrandolima16@gmail.com>
2023-11-12 17:05:02 -0300

    Refatorado serviço de login e criado repositório

--- src/services/auth/AuthService.js
+++ src/services/auth/AuthService.js
@@ -1,42 +1,67 @@
-import api from '@/server/api';
+import AuthRepository from '@/repositories/AuthRepository';
 import { setAuth, removeAuth } from '@/storages/AuthStorage';
 import { messages } from '@/utils/messages/Message';
 
+export function statusCode(error) {
+    return messages(
+      error.response.data.status,
+      error.response.data.data,
+      error.response.data.message
+    );
+}
+
 export default class AuthService {
+    static async returnObjectAuth(user) {
+        if (user.data.status === 200) {
+            if (user.data.data.isAdmin == true) {
+                Swal.fire({
+                    icon: 'success',
+                    title: user.data.message,
+                }).then((result) => {
+                    if(result.isConfirmed) {
+                        window.location.reload(1);
+                    }
+                });
+                this.$router.push({name: 'dashboard'});
+            } else if (user.data.data.isAdmin == false) {
+                messages(
+                    user.data.status,
+                    user.data.data,
+                    user.data.message
+                );
+                this.$router.push({name: 'home'});
+            }
+        } else {
+            return user;
+        }
+    }
+
     static async login(body) {
         try {
-            const response = await api.post(`/auth/login`, body);
+            const response = await AuthRepository.login(body);
             setAuth(response.data.data);
-            return response.data;
+            return response;
         } catch (error) {
-            return messages(
-                error.response.data.status,
-                error.response.data.data,
-                error.response.data.message
-            );
+            return statusCode(error);
         }
     }
 
     static async logout() {
         try {
-            const response = await api.post(`/auth/logout`);
+            const response = await AuthRepository.logout();
             removeAuth();
             return response.data;
         } catch (error) {
-            return messages(
-                error.response.data.status,
-                error.response.data.data,
-                error.response.data.message
-            );
+            return statusCode(error);
         }
     }
 
     static async loginSocial(provider) {
         try {
-            const response = await api.get(`/auth/login/social/${provider}`);
+            const response = await AuthRepository.loginSocial(provider);
             //const response = await api.get(`/auth/login/social/${provider}/callback`);
-            window.localStorage.setItem('userAuth', JSON.stringify(response.data));
-            return response.data.message;
+            setAuth(response.data);
+            return response.data;
         } catch (error) {
             throw error;
         }
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -40,7 +40,6 @@ import Login from '@/components/login/Login.vue';
 import LoginSocial from '@/components/login/LoginSocial.vue';
 import ProductNewGrid from '@/components/product/client/ProductNewGrid.vue';
 import AuthService from '@/services/auth/AuthService';
-import { messages } from '@/utils/messages/Message';
 
 export default {
     components: { Banner, IForgotMyPassword, Login, LoginSocial, ProductNewGrid },
@@ -71,26 +70,7 @@ export default {
         },
         async auth() {
             const user = await AuthService.login(this.user);
-            if (user.data.isAdmin == true) {
-                Swal.fire({
-                    icon: 'success',
-                    title: user.message,
-                }).then((result) => {
-                    if(result.isConfirmed) {
-                        window.location.reload(1);
-                    }
-                });
-                this.$router.push({name: 'dashboard'});
-            } else if (user.data.isAdmin == false) {
-                messages(
-                    user.status,
-                    user.data,
-                    user.message
-                );
-                this.$router.push({name: 'home'});
-            } else {
-                this.errorList = user;
-            }
+            this.errorList = AuthService.returnObjectAuth(user);
         },
         async oAuth(providerName) {
             try {
